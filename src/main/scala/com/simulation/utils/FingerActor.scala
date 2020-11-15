413e52a8190ef63917d40c64166508b85bd7b210
aishwaryaSahani <asahan2@uic.edu>
2020-11-15 11:41:05 -0600

    FingerTable actor

---
+++ src/main/scala/com/simulation/utils/FingerActor.scala
@@ -0,0 +1,24 @@
+package com.simulation.utils
+
+import akka.actor.Actor
+import com.simulation.utils.FingerActor.{fetchFingerTable, updateFingerTable}
+
+import scala.collection.mutable
+
+class FingerActor extends Actor{
+
+  var finger_table = mutable.HashMap[Int,scala.collection.mutable.LinkedHashMap[Int, Int]]()
+
+  override def receive: Receive = {
+    case updateFingerTable(finger: scala.collection.mutable.LinkedHashMap[Int, Int], nodeIndex: Int) =>
+      finger_table(nodeIndex) = finger
+
+    case fetchFingerTable(nodeIndex: Int) =>
+      sender() ! finger_table(nodeIndex)
+  }
+}
+
+object FingerActor {
+  case class fetchFingerTable(nodeIndex: Int)
+  case class updateFingerTable(finger: scala.collection.mutable.LinkedHashMap[Int, Int], nodeIndex: Int)
+}
