658c805b736eb7807d36624c94ac03a9e7866c90
Ansul Goenka <agoenk2@uic.edu>
2020-11-18 19:33:01 -0600

    cassandra support added

---
+++ src/main/scala/com/simulation/cassandra/ConnectToCassandra.scala
@@ -0,0 +1,66 @@
+package com.simulation
+
+import com.datastax.driver.core.{Cluster, Session}
+import com.simulation.ActorDriver.logger
+import com.simulation.beans.EntityDefinition
+
+object ConnectToCassandra {
+  var cluster:Cluster = null
+  var session:Session = null
+
+  def setup(keyspaceName:String, host:String, port:Int) :(Cluster,Session) = {
+    cluster = Cluster.builder().addContactPoint(host).withPort(port).build()
+    session = cluster.connect()
+
+    var query = "DROP KEYSPACE IF EXISTS "+keyspaceName+";"
+    //Executing the query
+    session.execute(query)
+
+    //Query to create a keyspace
+    query = "CREATE KEYSPACE "+keyspaceName+" WITH replication = {'class':'SimpleStrategy', 'replication_factor':3};"
+    session.execute(query)
+
+    //Connect to the testkeyspace keyspace
+    session = cluster.connect(keyspaceName)
+
+
+    (cluster,session)
+  }
+
+  def close() = {
+    if( session != null ) session.close()
+    if( cluster != null ) cluster.close()
+  }
+
+  def createTable(): Unit ={
+    val keyspace = "nodes"
+    val (cluster, session) = ConnectToCassandra.setup(keyspace, "localhost", 9042)
+
+    //Create table
+    val query=  "CREATE TABLE stock(id int PRIMARY KEY, name text) IF NOT EXISTS;"
+    logger.info("Table created if it does not exist")
+    session.execute(query)
+  }
+
+  def addToCassandra(Data: EntityDefinition) = {
+    try {
+      val keyspace = "nodes"
+      val (cluster, session) = ConnectToCassandra.setup(keyspace, "localhost", 9042)
+
+      //Insert data into the table
+      var query = "INSERT INTO stock (id, name) VALUES("+Data.id+","+Data.name+");"
+      session.execute(query)
+
+      //Retrieve data from stock table
+      query = "SELECT * FROM stock;"
+      val result = session.execute(query)
+      logger.info("Column definition: " + result.getColumnDefinitions.toString)
+      logger.info("Fetching the stored data from Cassandra: " + result.all)
+
+    }
+    finally {
+      ConnectToCassandra.close()
+      println("Done!")
+    }
+  }
+}
